dd4c5850898fa0b9dbe5604aa2a3571188cd61d9
pavel-paulau <pavel.paulau@gmail.com>
2013-05-07 21:54:52 +0300

    emit keys in doc generator

--- spring/docgen.py
+++ spring/docgen.py
@@ -7,6 +7,7 @@ class DocGen(object):
 
     CHARS = list(string.letters + string.digits)
     SIZE_VARIATION = 0.25  # 25%
+    KEY_LENGTH = 10
 
     def __init__(self, avg_size):
         self.avg_size = avg_size
@@ -19,15 +20,20 @@ class DocGen(object):
         return random.uniform(1 - cls.SIZE_VARIATION, 1 + cls.SIZE_VARIATION)
 
     @classmethod
-    def _get_string(cls, length):
+    def _build_short_string(cls):
+        return ''.join(cls.CHARS)[-cls.KEY_LENGTH:]
+
+    @classmethod
+    def _build_long_string(cls, length):
         l_int = int(length)
         num_slices = int(math.ceil(length / len(cls.CHARS)))
-
-        random.shuffle(cls.CHARS)  # TODO: this is slow
         rand_chars = num_slices * cls.CHARS
-
         return ''.join(rand_chars)[:l_int]
 
     def next(self):
+        random.shuffle(self.CHARS)
+
         next_length = self._get_variation_coeff() * self.avg_size
-        return {self._get_string(10): self._get_string(next_length)}
+        key = self._build_short_string()
+        doc = {key: self._build_long_string(next_length)}
+        return key, doc
