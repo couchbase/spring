b6828088599ef6dd7cb96fc0e4f91a06b382138a
pavel-paulau <pavel.paulau@gmail.com>
2013-05-08 17:17:54 +0300

    do get commands in quite mode

--- spring/cbgen.py
+++ spring/cbgen.py
@@ -1,16 +1,13 @@
 from couchbase import Couchbase
 
 
-from docgen import DocGen
-
-
 class CBGen(object):
 
     def __init__(self, host, port, username, password, bucket):
         self.client = Couchbase.connect(host, port, username, password, bucket)
 
     def _do_get(self, key):
-        self.client.get(key)
+        self.client.get(key, quiet=True)
 
     def _do_set(self, key, doc):
         self.client.set(key, doc)
