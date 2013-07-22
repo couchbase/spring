df7688a9a8b41dac212e3f18b25d04cd966223f9
pavel-paulau <pavel.paulau@gmail.com>
2013-07-22 12:22:22 +0300

    set quiet mode globally

--- spring/cbgen.py
+++ spring/cbgen.py
@@ -1,23 +1,19 @@
 from couchbase import Couchbase
-from couchbase.exceptions import NotFoundError
 
 
 class CBGen(object):
 
     def __init__(self, *args, **kwargs):
-        self.client = Couchbase.connect(*args, **kwargs)
+        self.client = Couchbase.connect(*args, **kwargs, quiet=True)
 
     def create(self, key, doc):
         self.client.set(key, doc)
 
     def read(self, key):
-        self.client.get(key, quiet=True)
+        self.client.get(key)
 
     def update(self, key, doc):
         self.client.set(key, doc)
 
     def delete(self, key):
-        try:
-            self.client.delete(key)
-        except NotFoundError:
-            pass
+        self.client.delete(key)
