775a1cf1f08ed87d7b932c3473ba42d89e7482c2
pavel-paulau <pavel.paulau@gmail.com>
2013-07-22 12:32:33 +0300

    fix order of parameters

--- spring/cbgen.py
+++ spring/cbgen.py
@@ -4,7 +4,7 @@ from couchbase import Couchbase
 class CBGen(object):
 
     def __init__(self, *args, **kwargs):
-        self.client = Couchbase.connect(*args, **kwargs, quiet=True, timeout=60)
+        self.client = Couchbase.connect(*args, quiet=True, timeout=60, **kwargs)
 
     def create(self, key, doc):
         self.client.set(key, doc)
