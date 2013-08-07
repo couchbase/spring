f1d87f4c77ec00da871131f0a9941f59c25703ee
pavel-paulau <pavel.paulau@gmail.com>
2013-08-07 16:29:15 +0300

    revise objects and types in wgen module

--- spring/wgen.py
+++ spring/wgen.py
@@ -27,7 +27,7 @@ def with_sleep(method):
     return wrapper
 
 
-class WorkloadGen(object):
+class Worker(object):
 
     BATCH_SIZE = 100
 
@@ -36,7 +36,36 @@ class WorkloadGen(object):
         self.ts = target_settings
         self.shutdown_event = shutdown_event
 
-    def _gen_sequence(self):
+        self.existing_keys = ExistingKey(self.ws.working_set,
+                                         self.ws.working_set_access,
+                                         self.ts.prefix)
+        self.new_keys = NewKey(self.ts.prefix, self.ws.expiration)
+        self.keys_for_removal = KeyForRemoval(self.ts.prefix)
+        self.docs = NewDocument(self.ws.size)
+
+        self.next_report = 0.05  # report after every 5% of completion
+
+        self.init_db()
+
+    def init_db(self):
+        try:
+            host, port = self.ts.node.split(':')
+            self.cb = CBGen(self.ts.bucket, host, port,
+                            self.ts.username, self.ts.password)
+        except Exception as e:
+            raise SystemExit(e)
+
+    def report_progress(self, curr_ops):
+        if self.ws.ops < float('inf') and \
+                curr_ops > self.next_report * self.ws.ops:
+            progress = 100.0 * curr_ops / self.ws.ops
+            self.next_report += 0.05
+            logger.info('Current progress: {0:.2f} %'.format(progress))
+
+
+class KVWorker(Worker):
+
+    def gen_sequence(self):
         ops = \
             ['c'] * self.ws.creates + \
             ['r'] * self.ws.reads + \
@@ -45,15 +74,8 @@ class WorkloadGen(object):
         random.shuffle(ops)
         return ops
 
-    def _report_progress(self, curr_ops):
-        if self.ws.ops < float('inf') and \
-                curr_ops > self.next_report * self.ws.ops:
-            progress = 100.0 * curr_ops / self.ws.ops
-            self.next_report += 0.05
-            logger.info('Current progress: {0:.2f} %'.format(progress))
-
     @with_sleep
-    def _do_batch(self, curr_items, deleted_items, lock, target_time):
+    def do_batch(self, curr_items, deleted_items, lock, target_time):
         curr_items_tmp = curr_items.value
         deleted_items_tmp = deleted_items.value
         if self.ws.creates:
@@ -63,7 +85,7 @@ class WorkloadGen(object):
             with lock:
                 deleted_items.value += self.ws.deletes
 
-        for i, op in enumerate(self._gen_sequence()):
+        for i, op in enumerate(self.gen_sequence()):
             if op == 'c':
                 curr_items_tmp += 1
                 key, ttl = self.new_keys.next(curr_items_tmp)
@@ -81,40 +103,7 @@ class WorkloadGen(object):
                 key = self.keys_for_removal.next(deleted_items_tmp)
                 self.cb.delete(key)
 
-    def _run_worker(self, sid, *args):
-        try:
-            host, port = self.ts.node.split(':')
-            self.cb = CBGen(self.ts.bucket, host, port,
-                            self.ts.username, self.ts.password)
-        except Exception as e:
-            raise SystemExit(e)
-
-        if self.ws.seq_updates:
-            self._do_seq_updates(sid)
-        if self.ws.seq_reads:
-            self._do_seq_reads(sid)
-        if not (self.ws.seq_updates or self.ws.seq_reads):
-            self._run_workload(sid, *args)
-
-    def _do_seq_reads(self, sid):
-        for key in SequentialHotKey(sid, self.ws, self.ts.prefix):
-            self.cb.read(key)
-
-    def _do_seq_updates(self, sid):
-        self.docs = NewDocument(self.ws.size)
-        for key in SequentialHotKey(sid, self.ws, self.ts.prefix):
-            doc = self.docs.next(key)
-            self.cb.update(key, doc)
-
-    def _run_workload(self, sid, lock, curr_ops, curr_items, deleted_items):
-        self.existing_keys = ExistingKey(self.ws.working_set,
-                                         self.ws.working_set_access,
-                                         self.ts.prefix)
-        self.new_keys = NewKey(self.ts.prefix, self.ws.expiration)
-        self.keys_for_removal = KeyForRemoval(self.ts.prefix)
-        self.docs = NewDocument(self.ws.size)
-
-        self.next_report = 0.05  # report after every 5% of completion
+    def run(self, sid, lock, curr_ops, curr_items, deleted_items):
         if self.ws.throughput < float('inf'):
             target_time = float(self.BATCH_SIZE) * self.ws.workers / \
                 self.ws.throughput
@@ -126,11 +115,11 @@ class WorkloadGen(object):
                 with lock:
                     curr_ops.value += self.BATCH_SIZE
 
-                self._do_batch(curr_items, deleted_items, lock,
-                               target_time=target_time)
+                self.do_batch(curr_items, deleted_items, lock,
+                              target_time=target_time)
 
                 if not sid:  # only first worker
-                    self._report_progress(curr_ops.value)
+                    self.report_progress(curr_ops.value)
 
                 if self.shutdown_event is not None and \
                         self.shutdown_event.is_set():
@@ -140,20 +129,57 @@ class WorkloadGen(object):
         else:
             logger.info('Finished: worker-{0}'.format(sid))
 
+
+class SeqReadsWorker(KVWorker):
+
+    def run(self, sid, *args, **kwargs):
+        for key in SequentialHotKey(sid, self.ws, self.ts.prefix):
+            self.cb.read(key)
+
+
+class SeqUpdatesWorker(KVWorker):
+
+    def run(self, sid, *args, **kwargs):
+        for key in SequentialHotKey(sid, self.ws, self.ts.prefix):
+            doc = self.docs.next(key)
+            self.cb.update(key, doc)
+
+
+class WorkerFactory(object):
+
+    def __new__(self, seq_updates, seq_reads):
+
+        if seq_updates:
+            return SeqUpdatesWorker
+        if seq_reads:
+            return SeqReadsWorker
+        if not (seq_updates or seq_reads):
+            return KVWorker
+
+
+class WorkloadGen(object):
+
+    def __init__(self, workload_settings, target_settings, shutdown_event=None):
+        self.ws = workload_settings
+        self.ts = target_settings
+        self.shutdown_event = shutdown_event
+
     def run(self):
-        workers = list()
         curr_items = Value('i', self.ws.items)
         deleted_items = Value('i', 0)
         curr_ops = Value('i', 0)
         lock = Lock()
 
+        worker_type = WorkerFactory(self.ws.seq_updates, self.ws.seq_reads)
+        workers = list()
         for sid in range(self.ws.workers):
-            worker = Process(
-                target=self._run_worker,
+            worker = worker_type(self.ws, self.ts, self.shutdown_event)
+            worker_process = Process(
+                target=worker.run,
                 args=(sid, lock, curr_ops, curr_items, deleted_items)
             )
-            worker.start()
-            workers.append(worker)
+            worker_process.start()
+            workers.append(worker_process)
 
         for worker in workers:
             worker.join()
