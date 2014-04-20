30786be7c9c51fe8c7a953ed415e601d307f5c2e
pavel-paulau <pavel.paulau@gmail.com>
2014-04-20 15:01:59 -0700

    fix typo in view names

--- spring/querygen.py
+++ spring/querygen.py
@@ -13,13 +13,13 @@ class NewQuery(object):
 
     QUERIES_PER_VIEW = {
         'id_by_city': 9,
-        'name_and_email_by_category_and_and_coins': 6,
+        'name_and_email_by_category_and_coins': 6,
         'id_by_realm_and_coins': 5,
         'name_and_email_by_city': 9,
-        'name_by_category_and_and_coins': 6,
+        'name_by_category_and_coins': 6,
         'experts_id_by_realm_and_coins': 5,
         'id_by_realm': 9,
-        'achievements_by_category_and_and_coins': 6,
+        'achievements_by_category_and_coins': 6,
         'name_and_email_by_realm_and_coins': 5,
         'experts_coins_by_name': 9,
     }
