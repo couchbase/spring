899519a62fe75a2974704398dcf0fd05f58f9da5
Pavel <pavel.paulau@gmail.com>
2016-06-27 11:23:09 -0700

    Upgrade spring to use Couchbase Python SDK 2.1
    Change-Id: I14796e22b9d28e44ee13051abfc3a74e986ef27c
    Reviewed-on: http://review.couchbase.org/65264
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- setup.py
+++ setup.py
@@ -9,19 +9,22 @@ setup(
     name='spring',
     version=VERSION,
     description='Simple Couchbase workload generator based on pylibcouchbase',
-    author='Pavel Paulau',
-    author_email='pavel.paulau@gmail.com',
     packages=['spring'],
     entry_points={
         'console_scripts': ['spring = spring.__main__:main']
     },
     install_requires=[
-        'couchbase==1.2.1',
-        'decorator',
-        'logger',
-        'numpy',
+        'couchbase==2.1.0',
+        'decorator==4.0.10',
+        'logger==1.4',
+        'numpy==1.11.0',
         'requests==2.1.0',
-        'twisted==12.0.0',
+        'urllib3==1.10.4',
+        'twisted==16.2.0',
+        'dcp-client'
+    ],
+    dependency_links=[
+        'git+https://github.com/couchbaselabs/python-dcp-client.git#egg=dcp-client'
     ],
     ext_modules=[
         fastdocgen
